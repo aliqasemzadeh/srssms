71a6f8cd42a34297e71a6a24ffdc2e7e832758ac
Ali Qasemzadeh <aliqasemzadeh7@gmail.com>
2026-07-21 13:20:40 +0330

    Extract shared head and foot sections into reusable Blade partials

--- resources/views/layouts/auth.blade.php
+++ resources/views/layouts/auth.blade.php
@@ -1,19 +1,9 @@
 <!DOCTYPE html>
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}" dir="{{ __('general.direction') }}">
-<head>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-
-    <title>{{ $title ?? config('app.name') }}</title>
-
-    @vite(['resources/css/app.css', 'resources/js/app.js'])
-
-    @fluxAppearance
-</head>
+@include('layouts.shared.head')
 <body>
 {{ $slot }}
 
-@livewireScripts
-@fluxScripts
+@include('layouts.shared.foot')
 </body>
 </html>
