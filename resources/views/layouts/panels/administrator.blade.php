206139b5ed3b9a19e5ec6115df77f47237f7b91e
Ali Qasemzadeh <aliqasemzadeh7@gmail.com>
2026-07-22 14:56:43 +0330

    Add `users` icon view and refine sidebar panels: improve localization, update route definitions, and enhance `user_management` group structure.

--- resources/views/layouts/panels/administrator.blade.php
+++ resources/views/layouts/panels/administrator.blade.php
@@ -1,16 +1,22 @@
 <flux:sidebar.header>
     <flux:sidebar.brand
-        href="#"
+        href="{{ route('panels.administrator.dashboard.index') }}"
         logo="https://fluxui.dev/img/demo/logo.png"
         logo:dark="https://fluxui.dev/img/demo/dark-mode-logo.png"
         name="Acme Inc."
     />
     <flux:sidebar.collapse class="lg:hidden" />
 </flux:sidebar.header>
-<flux:sidebar.search placeholder="Search..." />
+<flux:sidebar.search placeholder="{{ __('general.search') }}..." />
 <flux:sidebar.nav>
     <flux:sidebar.item icon="home" href="{{ route('panels.administrator.dashboard.index') }}" :current="request()->routeIs('panels.administrator.dashboard.index')">{{ __('general.dashboard') }}</flux:sidebar.item>
-    <flux:sidebar.group expandable heading="{{ __('general.user_management') }}" class="grid" :open="request()->is('panels/administrator/user-management*')">
+    <flux:sidebar.group
+        expandable
+        icon="users"
+        heading="{{ __('general.user_management') }}"
+        class="grid"
+        :expanded="request()->routeIs('panels.administrator.user-management.*')"
+    >
         <flux:sidebar.item href="{{ route('panels.administrator.user-management.user.index') }}" :current="request()->routeIs('panels.administrator.user-management.user.index')">{{ __('general.users') }}</flux:sidebar.item>
         <flux:sidebar.item href="{{ route('panels.administrator.user-management.role.index') }}" :current="request()->routeIs('panels.administrator.user-management.role.index')">{{ __('general.roles') }}</flux:sidebar.item>
         <flux:sidebar.item href="{{ route('panels.administrator.user-management.permission.index') }}" :current="request()->routeIs('panels.administrator.user-management.permission.index')">{{ __('general.permissions') }}</flux:sidebar.item>
--- resources/views/layouts/panels/user.blade.php
+++ resources/views/layouts/panels/user.blade.php
@@ -1,21 +1,13 @@
 <flux:sidebar.header>
     <flux:sidebar.brand
-        href="#"
+        href="{{ route('panels.user.dashboard.index') }}"
         logo="https://fluxui.dev/img/demo/logo.png"
         logo:dark="https://fluxui.dev/img/demo/dark-mode-logo.png"
         name="Acme Inc."
     />
     <flux:sidebar.collapse class="lg:hidden" />
 </flux:sidebar.header>
-<flux:sidebar.search placeholder="Search..." />
+<flux:sidebar.search placeholder="{{ __('general.search') }}..." />
 <flux:sidebar.nav>
     <flux:sidebar.item icon="home" href="{{ route('panels.user.dashboard.index') }}" :current="request()->routeIs('panels.user.dashboard.index')">{{ __('general.dashboard') }}</flux:sidebar.item>
-    <flux:sidebar.item icon="inbox" badge="12" href="#">{{ __('general.inbox') }}</flux:sidebar.item>
-    <flux:sidebar.item icon="document-text" href="#">{{ __('general.documents') }}</flux:sidebar.item>
-    <flux:sidebar.item icon="calendar" href="#">{{ __('general.calendar') }}</flux:sidebar.item>
-    <flux:sidebar.group expandable heading="{{ __('general.favorites') }}" class="grid">
-        <flux:sidebar.item href="#">{{ __('general.marketing_site') }}</flux:sidebar.item>
-        <flux:sidebar.item href="#">{{ __('general.android_app') }}</flux:sidebar.item>
-        <flux:sidebar.item href="#">{{ __('general.brand_guidelines') }}</flux:sidebar.item>
-    </flux:sidebar.group>
 </flux:sidebar.nav>
